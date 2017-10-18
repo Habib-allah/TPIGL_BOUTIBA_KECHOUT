917ce65dbc3ea434f1fa58096fb38de2e513f217
Habib-allah <fm_kechout@esi.dz>
2017-10-18 08:51:34 +0200

    Tests 0.1

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -96,6 +96,7 @@ public class VectorHelper {
             System.out.println("min = "+ min);
 
         }
+        return maxmin;
     }
     public int f(int v){
         return v*2;
@@ -104,7 +105,7 @@ public class VectorHelper {
         for(int i=0;i<vector.length;i++){
             vector[i]=f(vector[i]);
         }
-        return maxmin;
+
     }
 
 }
