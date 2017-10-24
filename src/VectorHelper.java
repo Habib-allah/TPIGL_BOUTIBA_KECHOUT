548fd3e3f43b12b17754f15f2398ee0550ca6f8e
Habib-allah <fm_kechout@esi.dz>
2017-10-24 19:56:15 +0200

    ..

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 public class VectorHelper {
     private int vector[];
 
-    /**
-     * Constructeur sans parametres au cas ou l'utilisaeur veut entrer une taille precise pour le tableau VECTOR
-     */
     public VectorHelper() {
         System.out.print("Entrer la taille du tableau : ");
         Scanner sc = new Scanner(System.in);
@@ -20,35 +17,21 @@ public class VectorHelper {
         }
     }
 
-    /**
-     * Un deuxieme constructeur !
-     * @param vector tableau qui va etre associé à un objet VectorHelper
-     */
     public VectorHelper(int[] vector){
         this.vector = vector;
     }
 
-
-    /**
-     * Setter
-     * @param vector tableau qui va etre associé à un objet VectorHelper
-     */
     public void setVector(int[] vector){
         this.vector = vector;
     }
 
-    /**
-     * Getter
-     * @return l'attribut vector d'un objet VectorHelper..
-     */
     public int[] getVector(){
         return vector;
     }
 
-    /**
-     * Tri par "Selection" d'un tableau
-     */
+
     public void trier() {
+        //SELECTION SORT
         for (int i = 0; i < vector.length - 1; i++) {
             int ind = i;
             for (int j = i + 1; j < vector.length; j++) {
@@ -71,12 +54,6 @@ public class VectorHelper {
         }
     }
 
-
-    /**
-     *  Somme le vecteur de l'objet courant avec le vecteur de l'objet v2
-      * @param v2 objet VectorHelper dans lequel on trouve le vecteur a sommer avec le vecteur de l'objet courant
-     * @throws DiffTailleException si les tailles des deux tables sont differents
-     */
     public int[] somme(VectorHelper v2) throws DiffTailleException{
         int[] v3 = null;
 
@@ -91,14 +68,10 @@ public class VectorHelper {
         return v3;
     }
 
-    /**
-     *Calcule le max et le min de l'attribut vecteur simultanement
-     * @return Tableau de deux elements : 1er max , 2eme min
-     */
     public int[] minmax(){
         int maxmin[] = null;
         if(vector != null && vector.length>0) {
-             maxmin= new int[2];
+            maxmin= new int[2];
             int max = vector[0], min = vector[0];
             for (int i = 0; i < vector.length; i++) {
                 if(vector[i]>max) max = vector[i];
@@ -110,6 +83,7 @@ public class VectorHelper {
         }
         return maxmin;
     }
+
     public int f(int v){
         return v*2;
     }
