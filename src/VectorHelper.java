907d8924572697266cde7b7914dbd12b3a27e6d2
Kariiim <fk_boutiba@esi.dz>
2017-10-24 20:16:01 +0100

    reload

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class VectorHelper {
     private int vector[];
 
+    /**
+     * Constructeur sans parametres au cas ou l'utilisaeur veut entrer une taille precise pour le tableau VECTOR
+     */
     public VectorHelper() {
         System.out.print("Entrer la taille du tableau : ");
         Scanner sc = new Scanner(System.in);
@@ -17,21 +20,35 @@ public class VectorHelper {
         }
     }
 
+    /**
+     * Un deuxieme constructeur !
+     * @param vector tableau qui va etre associé à un objet VectorHelper
+     */
     public VectorHelper(int[] vector){
         this.vector = vector;
     }
 
+
+    /**
+     * Setter
+     * @param vector tableau qui va etre associé à un objet VectorHelper
+     */
     public void setVector(int[] vector){
         this.vector = vector;
     }
 
+    /**
+     * Getter
+     * @return l'attribut vector d'un objet VectorHelper..
+     */
     public int[] getVector(){
         return vector;
     }
 
-
+    /**
+     * Tri par "Selection" d'un tableau
+     */
     public void trier() {
-        //SELECTION SORT
         for (int i = 0; i < vector.length - 1; i++) {
             int ind = i;
             for (int j = i + 1; j < vector.length; j++) {
@@ -44,6 +61,9 @@ public class VectorHelper {
             vector[i] = min;
         }
     }
+    /**
+     *Inverse les elements de vector par exemple le 1er element devient le dernier et l'inverse
+     */
 
     public void inverser() {
         int m;
@@ -54,6 +74,12 @@ public class VectorHelper {
         }
     }
 
+
+    /**
+     *  Somme le vecteur de l'objet courant avec le vecteur de l'objet v2
+     * @param v2 objet VectorHelper dans lequel on trouve le vecteur a sommer avec le vecteur de l'objet courant
+     * @throws DiffTailleException si les tailles des deux tables sont differents
+     */
     public int[] somme(VectorHelper v2) throws DiffTailleException{
         int[] v3 = null;
 
@@ -69,6 +95,10 @@ public class VectorHelper {
         return v3;
     }
 
+    /**
+     *Calcule le max et le min de l'attribut vecteur simultanement
+     * @return Tableau de deux elements : 1er max , 2eme min
+     */
     public int[] minmax(){
         int maxmin[] = null;
         if(vector != null && vector.length>0) {
@@ -84,10 +114,19 @@ public class VectorHelper {
         }
         return maxmin;
     }
-
+    /**
+     * La formule a appliquer sur les elements de vector dans la methode appliquerFormule
+     * @param v element sur lequel on applique la formule
+     * @return f(v)
+     */
     public int f(int v){
         return v*2;
     }
+
+
+    /**
+     * Appliquer la formule f a tous les elements du vecteur vector
+     */
     public void appliquerFormule(){
         for(int i=0;i<vector.length;i++){
             vector[i]=f(vector[i]);
@@ -96,8 +135,4 @@ public class VectorHelper {
     }
 
 }
-
-
-
-
 
