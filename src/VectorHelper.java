8f12f499cee40bcf56f3f9c020b16616b89ad68a
Habib-allah <fm_kechout@esi.dz>
2017-10-24 20:39:09 +0200

    .

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -74,7 +74,6 @@ public class VectorHelper {
 
     /**
      *  Somme le vecteur de l'objet courant avec le vecteur de l'objet v2
-      * @param v2 objet VectorHelper dans lequel on trouve le vecteur a sommer avec le vecteur de l'objet courant
      * @throws DiffTailleException si les tailles des deux tables sont differents
      */
     public int[] somme(VectorHelper v2) throws DiffTailleException{
@@ -98,7 +97,6 @@ public class VectorHelper {
     public int[] minmax(){
         int maxmin[] = null;
         if(vector != null && vector.length>0) {
-             maxmin= new int[2];
             int max = vector[0], min = vector[0];
             for (int i = 0; i < vector.length; i++) {
                 if(vector[i]>max) max = vector[i];
@@ -121,8 +119,3 @@ public class VectorHelper {
     }
 
 }
-
-
-
-
-
--- src/VectorHelperTest.java
+++ src/VectorHelperTest.java
@@ -5,43 +5,50 @@ import static org.junit.Assert.*;
 public class VectorHelperTest {
     @Test
     public void trier() throws Exception {
-        VectorHelper v = new VectorHelper(new int[]{5,1,7,5,8});
+        VectorHelper v = new VectorHelper(new int[]{5, 1, 7, 5, 8});
         v.trier();
-        assertArrayEquals("non trié1",new int[]{1,5,5,7,8},v.getVector());
+        assertArrayEquals("non trié1", new int[]{1, 5, 5, 7, 8}, v.getVector());
 
-        v.setVector(new int[]{1,1,-5,1,2,-8});
+        v.setVector(new int[]{1, 1, -5, 1, 2, -8});
         v.trier();
-        assertArrayEquals("non trié2",new int[]{-8,-5,1,1,1,2},v.getVector());
+        assertArrayEquals("non trié2", new int[]{-8, -5, 1, 1, 1, 2}, v.getVector());
 
     }
 
     @Test(expected = DiffTailleException.class)
-    public void sommeTest () throws Exception{
-        VectorHelper v1 = new VectorHelper(new int[]{5,3,7,0,8});
-        VectorHelper v2 = new VectorHelper(new int[]{7,1,2,1,9});
+    public void sommeTest() throws Exception {
+        VectorHelper v1 = new VectorHelper(new int[]{5, 3, 7, 0, 8});
+        VectorHelper v2 = new VectorHelper(new int[]{7, 1, 2, 1, 9});
         int v3[] = v1.somme(v2);
-        assertArrayEquals("Somme erronée",new int[] {12,4,9,1,17},v3);
+        assertArrayEquals("Somme erronée", new int[]{12, 4, 9, 1, 17}, v3);
 
-        VectorHelper v4 = new VectorHelper(new int[]{5,3,7,0,8,6});
-        VectorHelper v5 = new VectorHelper(new int[]{7,1,2,1,9});
+        VectorHelper v4 = new VectorHelper(new int[]{5, 3, 7, 0, 8, 6});
+        VectorHelper v5 = new VectorHelper(new int[]{7, 1, 2, 1, 9});
         int v6[] = v4.somme(v5);
-        assertArrayEquals("Somme erronée",null,v6);
+        assertArrayEquals("Somme erronée", null, v6);
     }
 
     @Test
-    public void minMaxTest(){
-        VectorHelper v = new VectorHelper(new int[]{7,1,2,1,9});
-        int[] result =v.minmax();
-        assertArrayEquals("Le min ou le max est erroné !",new int[]{9,1},result);
+    public void minMaxTest() {
+        VectorHelper v = new VectorHelper(new int[]{7, 1, 2, 1, 9});
+        int[] result = v.minmax();
+        assertArrayEquals("Le min ou le max est erroné !", new int[]{9, 1}, result);
     }
 
     @Test
     public void inverser() throws Exception {
-        VectorHelper v = new VectorHelper(new int[]{5,6,7,5,8});
+        VectorHelper v = new VectorHelper(new int[]{5, 6, 7, 5, 8});
         v.inverser();
-        assertArrayEquals("not inversed1",new int[]{8,5,7,6,5},v.getVector());
-        v.setVector(new int[]{5,6,7,5});
+        assertArrayEquals("not inversed1", new int[]{8, 5, 7, 6, 5}, v.getVector());
+        v.setVector(new int[]{5, 6, 7, 5});
         v.inverser();
-        assertArrayEquals("not inversed2",new int[]{5,7,6,5},v.getVector());
+        assertArrayEquals("not inversed2", new int[]{5, 7, 6, 5}, v.getVector());
+    }
+
+    @Test
+    public void AppliquerFormule() throws Exception {
+        VectorHelper v = new VectorHelper(new int[]{1, 2, 3, 9, 6});
+        v.appliquerFormule();
+        assertArrayEquals("not applyed", new int[]{v.f(1), v.f(2), v.f(3), v.f(9), v.f(6)}, v.getVector());
     }
 }
